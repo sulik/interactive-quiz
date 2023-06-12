a8e5792c414463aa61ad2beba1627b178210e49d
Olav Viitmann <websulik@gmail.com>
2023-06-12 13:24:36 +0300

    Create base layout

---
+++ src/components/Layout.tsx
@@ -0,0 +1,31 @@
+import { ReactNode } from 'react';
+import { Box, Paper } from '@mui/material';
+
+export const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: '100vh',
+        p: 4,
+      }}>
+      <Paper
+        elevation={5}
+        sx={{
+          width: '100%',
+          maxWidth: '900px',
+          minHeight: '50vh',
+          p: 4,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          textAlign: 'center',
+        }}>
+        {children}
+      </Paper>
+    </Box>
+  );
+};
